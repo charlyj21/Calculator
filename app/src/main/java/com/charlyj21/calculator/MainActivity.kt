85f3a8ffb58553de0f823c26f6a7224274602319
carlosjfh <carlos.flores@unas.edu.pe>
2023-10-20 02:25:22 -0500

    App design

--- app/src/main/java/com/charlyj21/calculator/MainActivity.kt
+++ app/src/main/java/com/charlyj21/calculator/MainActivity.kt
@@ -3,14 +3,43 @@ package com.charlyj21.calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Refresh
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.charlyj21.calculator.ui.theme.CalculatorTheme
+import com.charlyj21.calculator.ui.theme.Cyan
+import com.charlyj21.calculator.ui.theme.Red
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,11 +49,136 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.primary
+                    color = MaterialTheme.colorScheme.secondary
                 ) {
+                    val calculatorButtons = remember {
+                        mutableStateListOf(
+                            CalculatorButton("AC", CalculatorButtonType.Reset),
+                            CalculatorButton("C", CalculatorButtonType.Action),
+                            CalculatorButton("%", CalculatorButtonType.Action),
+                            CalculatorButton("/", CalculatorButtonType.Action),
+                            CalculatorButton("7", CalculatorButtonType.Normal),
+                            CalculatorButton("8", CalculatorButtonType.Normal),
+                            CalculatorButton("9", CalculatorButtonType.Normal),
+                            CalculatorButton("x", CalculatorButtonType.Action),
+                            CalculatorButton("4", CalculatorButtonType.Normal),
+                            CalculatorButton("5", CalculatorButtonType.Normal),
+                            CalculatorButton("6", CalculatorButtonType.Normal),
+                            CalculatorButton("-", CalculatorButtonType.Action),
+                            CalculatorButton("1", CalculatorButtonType.Normal),
+                            CalculatorButton("2", CalculatorButtonType.Normal),
+                            CalculatorButton("3", CalculatorButtonType.Normal),
+                            CalculatorButton("+", CalculatorButtonType.Action),
 
+                            CalculatorButton(
+                                icon = Icons.Outlined.Refresh,
+                                type = CalculatorButtonType.Normal
+                            ),
+                            CalculatorButton("0", CalculatorButtonType.Normal),
+                            CalculatorButton(".", CalculatorButtonType.Normal),
+                            CalculatorButton("=", CalculatorButtonType.Action)
+                        )
+                    }
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            ,
+                        contentAlignment = Alignment.BottomCenter
+                    ) {
+                        LazyVerticalGrid(
+                            modifier = Modifier
+                                .clip(RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp))
+                                .background(MaterialTheme.colorScheme.primary)
+                                .padding(8.dp),
+                            columns = GridCells.Fixed(4),
+                            verticalArrangement = Arrangement.spacedBy(16.dp),
+                            horizontalArrangement = Arrangement.spacedBy(16.dp),
+                            contentPadding = PaddingValues(16.dp)
+                        ){
+                            items(calculatorButtons){
+                                CalcButton(
+                                    button = it,
+                                    OnClick = {
+
+                                    }
+                                )
+                            }
+                        }
+                    }
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopCenter){
+                        Row(
+                            horizontalArrangement = Arrangement.spacedBy(8.dp),
+                            modifier= Modifier
+                                .padding(horizontal = 8.dp, vertical = 4.dp)
+                                .clip(
+                                    RoundedCornerShape(8.dp)
+                                )
+                        ){
+                            Icon(
+                                modifier = Modifier.size(20.dp),
+                                painter = painterResource(id = R.drawable.ic_nightmode),
+                                contentDescription = null,
+                                tint = Color.White
+                            )
+                            Icon(
+                                modifier = Modifier.size(20.dp),
+                                painter = painterResource(id = R.drawable.ic_darkmode),
+                                contentDescription = null,
+                                tint = Color.White
+                            )
+                        }
+                    }
                 }
             }
         }
     }
+}
+
+@Composable
+fun CalcButton(button: CalculatorButton, OnClick : () -> Unit){
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(16.dp))
+            .background(MaterialTheme.colorScheme.secondary)
+            .fillMaxHeight()
+            .aspectRatio(1f)
+            .clickable {
+                OnClick()
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        val contentColor =
+            if (button.type == CalculatorButtonType.Normal)
+                Color.White
+            else if (button.type == CalculatorButtonType.Action)
+                Red
+            else
+                Cyan
+        if(button.text != null){
+            Text(
+                button.text,
+                color = contentColor,
+                fontWeight = FontWeight.Bold,
+                fontSize = if (button.type == CalculatorButtonType.Action) 25.sp else 20.sp,
+            )
+        }else{
+            Icon(
+                modifier = Modifier.size(32.dp),
+                imageVector = button.icon!!,
+                contentDescription = null,
+                tint = contentColor
+            )
+        }
+    }
+}
+
+data class CalculatorButton(
+    val text: String? = null,
+    val type: CalculatorButtonType,
+    val icon : ImageVector? = null
+)
+
+enum class CalculatorButtonType{
+    Normal, Action, Reset
 }
